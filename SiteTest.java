11df0b8c0d7d9edaa883bb318d7eae9530af1d30
B00409227 <127535780+B00409227@users.noreply.github.com>
2023-03-10 20:23:14 +0000

    Add files via upload

---
+++ SiteTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author MSI
+ */
+
+public class SiteTest {
+
+    public static void main(String[] args) {
+        String name = ("home");
+        Site site = new Site(name);
+        Integer option;
+        do {
+            System.out.println("0: quit");
+            System.out.println("1: add page to current page");
+            System.out.println("2: find page");
+            System.out.println("3: display current page");
+            System.out.println("4: display site page");
+            option = Input.getInteger("input option: ");
+            switch (option) {
+                case 1:
+                    name = Input.getString("input the page name: ");
+                    site.addSubpage(name);
+                    break;
+                case 2:
+                    name = Input.getString("input the home page name: ");
+                    try{
+                        site.findpage(name);
+                        System.out.println(site.getCurrentPage());
+                    } catch(Site.PageNotFoundException e){
+                        System.out.println("not found");
+                    } 
+                    break;
+                case 3:
+                    System.out.println(site.getCurrentPage());
+                    break;
+                case 4:
+                    System.out.println(site);
+                    break;
+            }
+        } while (option != 0);
+    }
+
+}
